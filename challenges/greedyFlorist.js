220fd61bc0e1a43ef3f4987da79f3f354f1aca4d
KennyKoyama <107780712+KennyKoyama@users.noreply.github.com>
2022-09-08 13:46:08 -0300

    incompleted

---
+++ challenges/greedyFlorist.js
@@ -0,0 +1,10 @@
+// Complete the getMinimumCost function below.
+function getMinimumCost(k, c) {
+    let result
+    if(c.length == k) return c.reduce((sum, curr) => sum + curr,0);
+    
+
+    return result
+}
+
+console.log(getMinimumCost(3,[1, 3, 5, 7, 9]))
